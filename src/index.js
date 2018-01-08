41da097e719c3ff166886072a9d3ad9cf189bc89
Sarah Strange <strange1120@gmail.com>
2018-01-07 20:13:24 -0500

    demo member list created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import routes from './routes'
+// import routes from './routes'
+import MemberList from './components/ui/MemberList'
 
 window.React = React
 
-render(routes, document.getElementById('react-container'))
+render(
+	<MemberList />, 
+	document.getElementById('react-container'))
+
